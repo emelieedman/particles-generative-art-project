f815b6dfe0109c5b9e3f46cfd855025f7a9f2b78
Emelie Edman <emelie@MacBook-Pro.local>
2021-04-25 11:57:26 +0200

    interactive paricle text

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particleArray = [];
+let adjustX = 25;
+let adjustY = 65;
 
 //handle mouse
 
 const mouse = {
   x: null,
   y: null,
-  radius: 150,
+  radius: 350,
 };
 
 window.addEventListener('mousemove', (event) => {
@@ -17,22 +19,22 @@ window.addEventListener('mousemove', (event) => {
   mouse.y = event.y;
 });
 
-ctx.fillStyle = 'white';
-ctx.font = '25px Verdana';
-ctx.fillText('DEVVINE', 0, 40);
-const textCoordinates = ctx.getImageData(0, 0, 100, 100);
+ctx.fillStyle = 'pink';
+ctx.font = '54px Verdana';
+ctx.fillText('DE</>INE', 10, 45);
+const textCoordinates = ctx.getImageData(0, 0, 1000, 1000);
 
 class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 3;
+    this.size = 1.5;
     this.baseX = this.x;
     this.baseY = this.y;
     this.density = Math.random() * 30 + 1;
   }
+
   draw() {
-    ctx.fillStyle = 'white';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.closePath();
@@ -42,26 +44,40 @@ class Particle {
     let dx = mouse.x - this.x;
     let dy = mouse.y - this.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
     let forceDirectionX = dx / distance;
     let forceDirectionY = dy / distance;
-    if (distance < 50) {
-      this.x += forceDirectionX;
-      this.y += forceDirectionY;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
+    if (distance < mouse.radius) {
+      this.x -= directionX;
+      this.y -= directionY;
     } else {
-      this.size = 3;
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+      if (this.y !== this.baseY) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 10;
+      }
     }
   }
 }
 
 const init = () => {
   particleArray = [];
-  for (let i = 0; i < 1000; i++) {
-    particleArray.push(
-      new Particle(
-        Math.random() * window.innerWidth,
-        Math.random() * window.innerHeight
-      )
-    );
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        let posX = x + adjustX;
+        let posY = y + adjustY;
+        particleArray.push(new Particle(posX * 5, posY * 5));
+      }
+    }
   }
 };
 
